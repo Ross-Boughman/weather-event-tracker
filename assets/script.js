634989f8750a8a457415d039fbc644aac1574b70
JSlander <jhovanslander@hotmail.com>
2023-01-23 21:40:34 -0600

    connected EONET (mostly)(probably)

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,36 @@
-var catUrl = "https://eonet.gsfc.nasa.gov/api/v2.1/categories";
-var layUrl = "https://eonet.gsfc.nasa.gov/api/v2.1/layers";
-var eveUrl = "https://eonet.gsfc.nasa.gov/api/v2.1/events";
-function getApi() {
-  fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs_per_page=10")
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
-function getApi() {
-  fetch("https://eonet.gsfc.nasa.gov/api/v2.1/layers?api_key=tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs_per_page=10")
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
-function getApi() {
-  fetch("https://eonet.gsfc.nasa.gov/api/v2.1/catergories?api_key=tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs_per_page=10")
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
-function getApi() {
-  fetch("https://api.nasa.gov/planetary/earth/imagery?api_key=tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs")
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
-getApi(catUrl);
-getApi(layUrl);
-getApi(eveUrl);
-// https://api.nasa.gov/planetary/apod?api_key=tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs
+let date = dayjs().format("ddd, MMM D, YYYY h:mm A");
+const elem = document.querySelector('#testtext')
+elem.innerText = date
+let api = "tPrhy7CjHoxq1fl3lMARW0hvdCS0OyKHAIqP80Cs";
+let catURL =
+  "https://eonet.gsfc.nasa.gov/api/v2.1/categories?api_key=" +
+  api;
+ fetch(catURL +  1,
+  ).then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+  });
+let eveURL =
+  "https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=" +
+  api;
+ fetch(eveURL +  1,
+  ).then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let title = data.events[0].title
+    console.log(data);
+    console.log(title);
+  });
+  let layURL =
+  "https://eonet.gsfc.nasa.gov/api/v2.1/layers?api_key=" +
+  api;
+ fetch(layURL +  1,
+  ).then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+  });
