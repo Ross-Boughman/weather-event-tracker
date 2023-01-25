d440bb881338588d8f2c4f2263eb095c6ed8b6ea
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-01-24 21:06:51 -0600

    revise earth.js with Teleport API to call images based on city names

--- assets/earth.js
+++ assets/earth.js
@@ -1,18 +1,46 @@
 let rootURL = "https://api.teleport.org/api/";
-let city = "/cities/";
+let city = "cities/";
+let urban = "urban_areas/";
+let image = "slug:";
+let call = "/images/";
 let cityURL = rootURL + city;
+let urbanURL = rootURL + urban + image + "albuquerque" + call;
 
-fetch(cityURL, {
-  method: "GET",
-  credentials: "same-origin",
-  redirect: "follow",
-})
-  .then(function (response) {
-    return response.json();
+let citySearch = function () {
+  fetch(cityURL, {
+    method: "GET",
+    credentials: "same-origin",
+    redirect: "follow",
   })
-  .then(function (data) {
-    console.log(data);
-    let cityList =
-      data["_embedded"]["city:search-results"][0].matching_full_name;
-    console.log(cityList);
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let cityList =
+        data["_embedded"]["city:search-results"][0].matching_full_name;
+      console.log(data);
+    });
+};
+
+let imgSearch = function (cityList) {
+  citySearch(cityList);
+  fetch(urbanURL, {
+    method: "GET",
+    credentials: "same-origin",
+    redirect: "follow",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let image = data["photos"][0]["image"]["mobile"];
+      console.log(image);
+      let newImg = document.createElement("img");
+      newImg.setAttribute("src", image);
+      let body = document.querySelector(".block");
+      body.appendChild(newImg);
+    });
+};
+
+imgSearch();
