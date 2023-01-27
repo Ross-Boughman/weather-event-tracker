f2e31d256c72d23063daea9e3d7b526ae48a28ed
JSlander <jhovanslander@hotmail.com>
2023-01-27 01:28:14 -0600

    populated text using for loop

--- assets/script.js
+++ assets/script.js
@@ -7,56 +7,51 @@ fetch(catURL + 1)
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    // console.log(data);
-  });
+  .then(function (data) {});
 let eveURL = "https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=" + api;
 fetch(eveURL + 1)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    let title0 = data.events[0].title;
-    localStorage.setItem("title-0", title0);
-    document.getElementById("event-title-0").innerHTML =
-      localStorage.getItem("title-0");
-    let title1 = data.events[1].title;
-    localStorage.setItem("title-1", title1);
-    document.getElementById("event-title-1").innerHTML =
-      localStorage.getItem("title-1");
-    let cat0 = data.events[0].categories[0].title;
-    localStorage.setItem("cat-0", cat0);
-    document.getElementById("event-category-0").innerHTML =
-      localStorage.getItem("cat-0");
-    let cat1 = data.events[1].categories[0].title;
-    localStorage.setItem("cat-1", cat1);
-    document.getElementById("event-category-1").innerHTML =
-      localStorage.getItem("cat-1");
-    let loc0 = data.events[0].geometries[0].coordinates;
-    localStorage.setItem("loc-0", loc0);
-    document.getElementById("event-location-0").innerHTML =
-      localStorage.getItem("loc-0");
-    let loc1 = data.events[1].geometries[0].coordinates;
-    localStorage.setItem("loc-1", loc1);
-    document.getElementById("event-location-1").innerHTML =
-      localStorage.getItem("loc-1");
-    let date0 = data.events[0].geometries[0].date;
-    localStorage.setItem("date-0", date0);
-    document.getElementById("event-date-0").innerHTML =
-      localStorage.getItem("date-0");
-    let date1 = data.events[1].geometries[0].date;
-    localStorage.setItem("date-1", date1);
-    document.getElementById("event-date-1").innerHTML =
-      localStorage.getItem("date-1");
-    let link0 = data.events[0].sources[0].url;
-    localStorage.setItem("link-0", link0);
-    document.getElementById("event-link-0").innerHTML =
-      localStorage.getItem("link-0");
-    let link1 = data.events[1].sources[0].url;
-    localStorage.setItem("link-1", link1);
-    document.getElementById("event-link-1").innerHTML =
-      localStorage.getItem("link-1");
+    let title = [];
+    let cat = [];
+    let loc = [];
+    let date = [];
+    let link = [];
+    for (i = 0; i < 5; i++) {
+      title[i] = data.events[i].title;
+      cat[i] = data.events[i].categories[0].title;
+      loc[i] = data.events[i].geometries[0].coordinates;
+      date[i] = data.events[i].geometries[0].date;
+      link[i] = data.events[i].sources[0].url;
+      localStorage.setItem("title-" + [i], title[i]);
+      localStorage.setItem("cat-" + [i], cat[i]);
+      localStorage.setItem("loc-" + [i], loc[i]);
+      localStorage.setItem("date-" + [i], date[i]);
+      localStorage.setItem("link-" + [i], link[i]);
+      document.getElementById("event-title-" + [i]).innerHTML =
+        localStorage.getItem("title-" + [i]);
+      document.getElementById("event-category-" + [i]).innerHTML =
+        localStorage.getItem("cat-" + [i]);
+      document.getElementById("event-location-" + [i]).innerHTML =
+        localStorage.getItem("loc-" + [i]);
+      document.getElementById("event-date-" + [i]).innerHTML =
+        localStorage.getItem("date-" + [i]);
+      document.getElementById("event-link-" + [i]).innerHTML =
+        localStorage.getItem("link-" + [i]);
+    }
+    dojo.addOnLoad(title[i]);
+    dojo.addOnLoad(cat[i]);
+    dojo.addOnLoad(loc[i]);
+    dojo.addOnLoad(date[i]);
+    dojo.addOnLoad(link[i]);
   });
+// document.getElementById("ice").addEventListener("click", );
+// document.getElementById("storm").addEventListener("click", );
+// document.getElementById("volcano").addEventListener("click", );
+// document.getElementById("wildfire").addEventListener("click", );
+// document.getElementById("homeBtn").addEventListener("click", );
 let layURL = "https://eonet.gsfc.nasa.gov/api/v2.1/layers?api_key=" + api;
 fetch(layURL + 1)
   .then(function (response) {
@@ -65,3 +60,7 @@ fetch(layURL + 1)
   .then(function (data) {
     // console.log(data);
   });
+// id 8 wildfires +
+// id 10 severe storms +
+// id 12 volcanoes +
+// id 15 sea and lake ice +
