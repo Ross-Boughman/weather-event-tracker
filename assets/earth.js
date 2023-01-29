dc23d0c4c0beca613e14a166f8357d2a8e21be72
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-01-29 16:38:57 -0600

    iterate through 5 images for 5 different weather events

--- assets/earth.js
+++ assets/earth.js
@@ -21,53 +21,54 @@ let citySearch = function () {
         let cities = data["_links"]["ua:item"][i]["name"]; // can change the array # to target a different city name
         let cityList = cities.toLowerCase(); // need to be lowercase for the image lookup
         cityArray.push(cityList);
-        imgSearch(cityList, cityArray);
         // pass the top 5 cities listed (alphabetical) to the imgSearch function for the .jpg
         // pass array of same 5 cities so able to iterate over the array as opposed to targetting individuals
       }
+      imgSearch(cityArray);
     });
 };
 
-let imgSearch = function (cityList, cityArray) {
-  let urbanURL = rootURL + urban + image + cityList + call;
-  fetch(urbanURL, {
-    method: "GET",
-    credentials: "same-origin",
-    redirect: "follow",
-  })
-    .then(function (response) {
-      return response.json();
+let imgSearch = function (cityArray) {
+  for (let i = 0; i < 5; i++) {
+    let name = cityArray[i];
+    let urbanURL = rootURL + urban + image + name + call;
+    fetch(urbanURL, {
+      method: "GET",
+      credentials: "same-origin",
+      redirect: "follow",
     })
-    .then(function (data) {
-      let image = data["photos"][0]["image"]["mobile"]; // URLs of .jpg image based on city name (cityList)
-      console.log(image);
-      let img = document.querySelector("#one");
-      let img2 = document.querySelector("#two");
-      let img3 = document.querySelector("#three");
-      let img4 = document.querySelector("#four");
-      let img5 = document.querySelector("#five");
-      img.setAttribute("src", image);
-      img2.setAttribute("src", image);
-      img3.setAttribute("src", image);
-      img4.setAttribute("src", image);
-      img5.setAttribute("src", image);
-      img.setAttribute("id", "cityimg");
-      img2.setAttribute("id", "cityimg");
-      img3.setAttribute("id", "cityimg");
-      img4.setAttribute("id", "cityimg");
-      img5.setAttribute("id", "cityimg");
-
-      let name = document.createElement("a");
-      name.innerHTML = cityList;
-      console.log(name);
-      img.appendChild(name);
-      img2.appendChild(name);
-      img3.appendChild(name);
-      img4.appendChild(name);
-      img5.appendChild(name);
-
-      // img.appendChild(newImg);
-    });
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let img = data["photos"][0]["image"]["mobile"]; // URLs of .jpg image based on city name (cityList)\
+        if (i === 0) {
+          let imagine = document.querySelector(".one");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        } else if (i === 1) {
+          let imagine = document.querySelector(".one");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        } else if (i === 2) {
+          let imagine = document.querySelector(".two");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        } else if (i === 3) {
+          let imagine = document.querySelector(".three");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        } else if (i === 4) {
+          let imagine = document.querySelector(".four");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        } else if (i === 5) {
+          let imagine = document.querySelector(".five");
+          imagine.setAttribute("id", "cityimg");
+          imagine.setAttribute("src", img);
+        }
+      });
+  }
 };
 
 citySearch();
